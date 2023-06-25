653de76427f523e09c3843f381adca6bddfe2ab6
Akshay Jain <akshayush007@gmail.com>
2023-06-25 17:13:38 +0530

    removingkey

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-openai_api_key="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"
+
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
